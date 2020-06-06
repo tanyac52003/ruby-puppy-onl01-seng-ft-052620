8aa8e0d3102bdee23a9e6e100c0722d7ca55f99a
Tanya Hodges <tanyac52003@gmail.com>
2020-06-06 20:02:22 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,27 @@
 # Add your code here
+
+class Dog 
+  attr_accessor :name 
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name 
+    @@all << self 
+  end 
+  
+  def self.all 
+    @@all 
+  end 
+  
+  def self.print_all
+    @@all.each do |dog|
+      puts self.name 
+    end 
+  end 
+  
+  def self.clear_all
+    @@all.clear
+  end 
+end 
+
